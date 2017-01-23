2cea7c5ea3a1dfb0fcdac7055cc97066abdd3169
Aditya Gudibanda <adityagudibanda1995@gmail.com>
2017-01-23 01:17:51 -0500

    Add files via upload
    Feedforward regular neural network with 2 perceptron layers. Validation that it does no better than random guessing.

---
+++ neural_net.py
@@ -0,0 +1,115 @@
+
+# Import MNIST data
+import tensorflow as tf
+import numpy as np
+
+
+from numpy import binary_repr
+
+
+sample_lengths = 30
+
+
+
+def make_samples(n):
+    x = []
+    for i in range(n):
+        #x.append(np.random.randint(2, size = sample_lengths))
+        x.append(map(int, list(binary_repr(np.random.randint(pow(2, sample_lengths), size = 1)[0], sample_lengths))))
+    y = []
+    for i in x:
+        if (sum(i) % 2) == 0:
+            y.append([1, 0])
+        else:
+            y.append([0, 1])
+
+    # print x
+    # print y
+    return x,y
+
+
+
+# Parameters
+learning_rate = 0.001
+training_epochs = 25
+batch_size = 5
+display_step = 1
+
+# Network Parameters
+n_hidden_1 = 256 # 1st layer number of features
+n_hidden_2 = 256 # 2nd layer number of features
+n_input = sample_lengths # MNIST data input (img shape: 28*28)
+n_classes = 2 # MNIST total classes (0-9 digits)
+
+# tf Graph input
+x = tf.placeholder("float", [None, n_input])
+y = tf.placeholder("float", [None, n_classes])
+
+
+# Create model
+def multilayer_perceptron(x, weights, biases):
+    # Hidden layer with RELU activation
+    layer_1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
+    layer_1 = tf.nn.relu(layer_1)
+    # Hidden layer with RELU activation
+    layer_2 = tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'])
+    layer_2 = tf.nn.relu(layer_2)
+    # Output layer with linear activation
+    out_layer = tf.matmul(layer_2, weights['out']) + biases['out']
+    return out_layer
+
+# Store layers weight & bias
+weights = {
+    'h1': tf.Variable(tf.random_normal([n_input, n_hidden_1])),
+    'h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2])),
+    'out': tf.Variable(tf.random_normal([n_hidden_2, n_classes]))
+}
+biases = {
+    'b1': tf.Variable(tf.random_normal([n_hidden_1])),
+    'b2': tf.Variable(tf.random_normal([n_hidden_2])),
+    'out': tf.Variable(tf.random_normal([n_classes]))
+}
+
+# Construct model
+pred = multilayer_perceptron(x, weights, biases)
+
+# Define loss and optimizer
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(pred, y))
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+
+# Initializing the variables
+init = tf.initialize_all_variables()
+
+test_x, test_y = make_samples(10000)
+
+# Launch the graph
+with tf.Session() as sess:
+    sess.run(init)
+
+    # Training cycle
+    for epoch in range(training_epochs):
+        avg_cost = 0.
+        total_batch = 10
+        # Loop over all batches
+        for i in range(10):
+            batch_x, batch_y = make_samples(batch_size)
+            # print batch_x
+            # print batch_y
+            # Run optimization op (backprop) and cost op (to get loss value)
+            _, c = sess.run([optimizer, cost], feed_dict={x: batch_x,
+                                                          y: batch_y})
+            # Compute average loss
+            avg_cost += c / total_batch
+        # Display logs per epoch step
+        if epoch % display_step == 0:
+            print("Epoch:", '%04d' % (epoch+1), "cost=", \
+                "{:.9f}".format(avg_cost))
+            correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
+            # Calculate accuracy
+            accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+            print("Accuracy:", accuracy.eval({x: test_x, y: test_y}))
+    print("Optimization Finished!")
+
+
+
+
